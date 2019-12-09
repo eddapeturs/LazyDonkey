0378127fb375adb9ad2b9ac26f6007a38665edd1
Straffern <14233825+Straffern@users.noreply.github.com>
2019-12-09 19:31:51 +0000

    Awesome

--- Assets/_Assets_LD/Scripts/wolfSpawn.cs
+++ Assets/_Assets_LD/Scripts/wolfSpawn.cs
@@ -4,62 +4,62 @@ using UnityEngine;
 
 public class wolfSpawn : MonoBehaviour
 {
-    [Header("Assets")]
-    public GameObject m_WolfPrefab = null;
+  [Header("Assets")]
+  public GameObject m_WolfPrefab = null;
 
-    Vector3 center;
-    private float timer;
+  Vector3 center;
+  private float timer;
 
-    int counter = 0;
+  int counter = 0;
 
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+    GetRandomTimer();
+    center = transform.position;
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    timer -= Time.deltaTime;
+
+    if (timer < 0)
     {
-        
-        GetRandomTimer();
-        center = transform.position;
+      CreateWolf();
+      GetRandomTimer();
 
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer -= Time.deltaTime;
+  private void CreateWolf()
+  {
+    int a = Random.Range(1, 360);
+    GameObject wolf;
+    Vector3 pos = RandomCircle(50.0f, a);
+    //wolf = Instantiate(m_WolfPrefab, gameObject.transform, false);
+    wolf = Instantiate(m_WolfPrefab, pos, Quaternion.identity);
+    //wolf.transform.position = new Vector3(pos.x + xAdd, pos.y + 1, pos.z + xAdd);
+    wolf.transform.Rotate(new Vector3(-90, 180, a));
+    wolf.transform.parent = transform;
+  }
 
-        if (timer < 0)
-        {
-            CreateWolf();
-            GetRandomTimer();
+  Vector3 RandomCircle(float radius, int a)
+  {
+    float ang = a;
+    Vector3 pos;
+    pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+    pos.y = center.y + 1;
+    pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+    //Debug.Log("Position: " + pos);
+    return pos;
+  }
 
-        }
-    }
-
-    private void CreateWolf()
-    {
-        int a = Random.Range(1, 360);
-        GameObject wolf;
-        Vector3 pos = RandomCircle(50.0f , a);
-        //wolf = Instantiate(m_WolfPrefab, gameObject.transform, false);
-        wolf = Instantiate(m_WolfPrefab, pos, Quaternion.identity);
-        //wolf.transform.position = new Vector3(pos.x + xAdd, pos.y + 1, pos.z + xAdd);
-        wolf.transform.Rotate(new Vector3(-90, 180, a));
-        wolf.transform.parent = transform;
-    }
-
-    Vector3 RandomCircle(float radius, int a)
-    {
-        float ang = a;
-        Vector3 pos;
-        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
-        pos.y = center.y + 1;
-        pos.z = center.z + radius* Mathf.Cos(ang * Mathf.Deg2Rad);
-        //Debug.Log("Position: " + pos);
-        return pos;
-    }
-
-    void GetRandomTimer()
-    {
-        timer = Random.Range(5f, 50f);
-    }
+  void GetRandomTimer()
+  {
+    timer = Random.Range(5f, 25f);
+  }
 }
