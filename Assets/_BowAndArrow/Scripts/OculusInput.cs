006f18d030f3a83b4189c251405f0f3ef3a3c2a6
Straffern <14233825+Straffern@users.noreply.github.com>
2019-12-11 01:25:40 +0000

    Spend 3 hours playing with an offset vector, discovering it's bugged.. Going to bed now, slightly depressed

--- Assets/_BowAndArrow/Scripts/OculusInput.cs
+++ Assets/_BowAndArrow/Scripts/OculusInput.cs
@@ -5,37 +5,54 @@ using UnityEngine;
 
 public class OculusInput : MonoBehaviour
 {
-  public Bow m_Bow = null;
+  private Bow m_Bow;
+  private GameObject _bow;
 
-  public GameObject player;
+  private GameObject player;
+  private GameObject leftHand;
+  private GameObject rightHand;
   private GameObject m_OppositeController;
   private OVRInput.Controller m_Controller;
 
 
   private void Awake()
   {
-    if (player == null)
-    {
-      player = GameObject.FindGameObjectWithTag("Player");
-    }
+    player = GameObject.FindGameObjectWithTag("Player");
+    m_Bow = gameObject.GetComponent(typeof(Bow)) as Bow;
+    _bow = GameObject.FindGameObjectWithTag("Bow");
+    leftHand = GameObject.FindGameObjectWithTag("LeftHand");
+    rightHand = GameObject.FindGameObjectWithTag("RightHand");
   }
 
   private void Update()
   {
-    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
-      m_Bow.Pull(m_OppositeController.transform);
+    if (_pickController())
+    {
+      if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+        m_Bow.Pull(m_OppositeController.transform);
 
-    if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
-      m_Bow.Release();
+      if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+        m_Bow.Release();
+    }
   }
 
 
-  private void _pickController()
-  {
-    if ()
-    {
 
+  private bool _pickController()
+  {
+    if (_bow.transform.IsChildOf(leftHand.transform))
+    {
+      m_Controller = OVRInput.Controller.RTouch;
+      m_OppositeController = rightHand;
+      return true;
     }
+    else if (_bow.transform.IsChildOf(leftHand.transform))
+    {
+      m_Controller = OVRInput.Controller.LTouch;
+      m_OppositeController = leftHand;
+      return true;
+    }
+    else return false;
   }
 
 }
