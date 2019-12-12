ee4300920e89fc95c94ea86c67f9a84c5c83485b
NjallMyrdal <33736143+NjallMyrdal@users.noreply.github.com>
2019-12-12 16:03:30 +0000

    sheep stuff

---
+++ Assets/_Assets_LD/Scripts/SheepHeadMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepHeadMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float loRanTime = 5f;
+    public float hiRanTime = 50f;
+    public Animation anim;
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play();
+    }
+}
