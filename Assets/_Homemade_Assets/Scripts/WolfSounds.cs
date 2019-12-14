9508b2d8f089b29fc972c731983136592fbf10de
Straffern <14233825+Straffern@users.noreply.github.com>
2019-12-14 18:05:00 +0000

    Wolf running sound

---
+++ Assets/_Homemade_Assets/Scripts/WolfSounds.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using OVR;
+
+public class WolfSounds : MonoBehaviour
+{
+  private SoundEmitter emitter;
+  public AudioClip running;
+  public AudioClip howl;
+  public AudioClip snare;
+  public NavMeshAgent agent;
+  public AudioSource audioSource;
+
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (agent.velocity != Vector3.zero)
+    {
+      if (!audioSource.isPlaying)
+      {
+        audioSource.clip = running;
+        audioSource.Play();
+      }
+    }
+    else if (audioSource.isPlaying && audioSource.clip == running)
+    {
+      audioSource.Stop();
+    }
+  }
+
+
+}
