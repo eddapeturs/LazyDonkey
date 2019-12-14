43451bdb7c7e818c48326a4772e1a1cfd7ca0260
Straffern <14233825+Straffern@users.noreply.github.com>
2019-12-14 20:40:36 +0000

    Spatialized wolf sounds, and imported new wolf prefab with animation.

--- Assets/_Homemade_Assets/Scripts/WolfHit.cs
+++ Assets/_Homemade_Assets/Scripts/WolfHit.cs
@@ -5,29 +5,28 @@ using UnityEngine;
 public class WolfHit : MonoBehaviour
 {
 
-  GameObject manager;
-  // Start is called before the first frame update
-  void Start()
-  {
-    manager = GameObject.Find("GameManager");
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-
-  void OnTriggerEnter(Collider col)
-  {
-    if (col.gameObject.tag == "FireArrow")
+    GameObject manager;
+    // Start is called before the first frame update
+    void Start()
     {
-
-      manager.GetComponent<GameManager>().updateWolfKill();
-      Destroy(col.gameObject, 0.2f);
-      Destroy(transform.parent.gameObject, 0.2f);
+        manager = GameObject.Find("GameManager");
     }
 
-  }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "FireArrow")
+        {
+            manager.GetComponent<GameManager>().updateWolfKill();
+            Destroy(col.gameObject, 0.2f);
+            Destroy(transform.parent.gameObject, 0.2f);
+        }
+        
+    }
 
 }
